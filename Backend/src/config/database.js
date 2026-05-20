5eb5eb2cbf763a1c35481519471de03606f4c957
QUY-FE <quysamp205@gmail.com>
2026-05-20 22:00:02 +0700

    sửa cấu trúc và tên file

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}`);
+        await mongoose.connect(process.env.MONGO_URI2 ||process.env.MONGO_URI);
         console.log("Kết nối thành công :))");
         
 
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import app from './app.js';
+dotenv.config();
 
 
 const port = process.env.PORT || 5000;
 connectDB();
-dotenv.config();
 
 
 app.listen(port, () => {
